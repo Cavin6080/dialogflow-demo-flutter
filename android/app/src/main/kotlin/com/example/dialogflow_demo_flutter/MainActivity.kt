e8b9d24afcfa5468e6f0c1b8887e595d9e51e226
cavin6080 <cavinmacwan1@gmail.com>
2023-02-18 19:01:10 +0530

    dialogflow demo in flutter

---
+++ android/app/src/main/kotlin/com/example/dialogflow_demo_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dialogflow_demo_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import dialogflow_grpc
+import speech_to_text_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DialogflowGrpcPlugin.register(with: registry.registrar(forPlugin: "DialogflowGrpcPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+}
